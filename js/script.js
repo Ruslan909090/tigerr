3546ce5378fc3b38dbc2ee723e9f1e3de396b90a
Ruslan909090 <tigerdnb@gmail.com>
2023-02-06 20:55:25 +0200

    tousch

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ window.addEventListener('DOMContentLoaded', () => {
     menuItem = document.querySelectorAll('.menu_item'),
     hamburger = document.querySelector('.hamburger');
 
-    hamburger.addEventListener('click', () => {
+    hamburger.addEventListener('touchstart', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menu_active');
     });
 
     menuItem.forEach(item => {
-        item.addEventListener('click', () => {
+        item.addEventListener('touchstart', () => {
             hamburger.classList.toggle('hamburger_active');
             menu.classList.toggle('menu_active');
         })
